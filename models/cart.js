0ed151ef9c0c6667c38afc33ce08b45afb23c161
sinhaji2000 <mukeshsinha@Mukeshs-MacBook-Air.local>
2024-04-18 19:35:02 +0530

    just finish the delete update operation

--- models/cart.js
+++ models/cart.js
@@ -43,17 +43,33 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-
-      const updateCart = { ...JSON.parse(fileContent) };
-      const product = updateCart.products.find(id);
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
       const productQty = product.qty;
-      updateCart.products = updateCart.products.filter((prod) => {
-        prod.id !== id;
-      });
-      updateCart.totalPrice = updateCart.totalPrice - productQty * productPrice;
-      fs.writeFile(p, JSON.stringify(updateCart), (err) => {
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if(err){
+        cb(null) ;
+      }else{
+        cb(cart);
+
+      }
     });
   }
 };
