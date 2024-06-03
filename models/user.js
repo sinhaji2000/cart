1362865f418faa6fdf93fd2d8528885b2d4085a7
sinhaji2000 <mukeshsinha@Mukeshs-MacBook-Air.local>
2024-06-04 00:20:37 +0530

    Storing Multiple Products in the Cart

--- models/user.js
+++ models/user.js
@@ -11,15 +11,25 @@ class User {
   }
 
   addToCart(product) {
-    // const cartProduct = this.cart.items.findIndex(cp => {
-    //       return cp._id === product._id ;
-    // })
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
 
-    // product.quantity = 1 ;
-    // console.log(product , 'addtocart , user.js')
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
 
     const updateCart = {
-      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDb();
     return db
